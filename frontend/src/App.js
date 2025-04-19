217b59e7d9163ff0d6840ee4cc2d174636f24d1d
kis12345678 <68807382+kis12345678@users.noreply.github.com>
2025-04-19 08:35:54 -0700

    Add frontend/src/App.js 1745076949089

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlightList from './components/FlightList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Airline Reservation System</h1>
+      <FlightList />
+    </div>
+  );
+}
+
+export default App;
